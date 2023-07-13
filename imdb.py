aff3002b1698d33e153a516d8e803f38e37e3fef
YousefMohamedSalah <ytmmng1@gmail.com>
2023-07-14 00:50:31 +0300

    Modfied

--- imdb.py
+++ imdb.py
@@ -7,3 +7,18 @@ Created on Tue Feb  7 16:42:15 2023
 
 import scrapy
 
+from scrapy.item import Item, Field
+
+from scrapy.loader import ItemLoader
+
+class IMDb_Movies(scrapy.Spider):
+    name = "imdb_movies"
+    def start_requests(self):
+    
+        start_urls = ["https://www.imdb.com/search/title/?genres=Action&sort=user_rating,desc&title_type=feature&num_votes=25000,&ref_=chttp_gnr_0"]
+    
+        for url in start_urls:
+            yield scrapy.Request(url = url, callback=self.parse)
+        
+    def parse(self,response):
+        pass
