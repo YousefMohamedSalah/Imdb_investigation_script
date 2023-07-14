e0c985bb991c1a85189e7bfce91dc6f54d134b53
YousefMohamedSalah <ytmmng1@gmail.com>
2023-07-14 03:12:33 +0300

    Modified

--- imdb.py
+++ imdb.py
@@ -11,11 +11,12 @@ from scrapy.item import Item, Field
 
 from scrapy.loader import ItemLoader
 
+
 class IMDb_Movies(scrapy.Spider):
     name = "imdb_movies"
     def start_requests(self):
     
-        start_urls = ["https://www.imdb.com/search/title/?genres=Action&sort=user_rating,desc&title_type=feature&num_votes=25000,&ref_=chttp_gnr_0"]
+        start_urls = ["https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres=action&sort=user_rating,desc&start=01&ref_=adv_nxt"]
     
         for url in start_urls:
             yield scrapy.Request(url = url, callback=self.parse)
